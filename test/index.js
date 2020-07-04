bbcb4048e80366cdfd6fde35a2d6df2c4d7dab58
Luke Edwards <luke.edwards05@gmail.com>
2020-07-03 20:45:39 -0700

    chore: add addl `prototype` pollution test

--- test/index.js
+++ test/index.js
@@ -126,12 +126,13 @@ Pollution('constructor', () => {
 
 	assert.notEqual(({})['a0'], true, 'Safe POJO');
 	assert.notEqual(new Object()['a0'], true, 'Safe Object');
+
 	assert.notEqual(input['a0'], true, 'Safe input');
 	assert.notEqual(output['a0'], true, 'Safe output');
 });
 
 // @see https://snyk.io/vuln/SNYK-JS-LODASH-450202
-Pollution('prototype', () => {
+Pollution('__proto__', () => {
 	const payload = '{"__proto__":{"a0":true}}';
 	const input = JSON.parse(payload);
 	const output = klona(input);
@@ -143,8 +144,26 @@ Pollution('prototype', () => {
 
 	assert.notEqual(({})['a0'], true, 'Safe POJO');
 	assert.notEqual(new Object()['a0'], true, 'Safe Object');
+
 	assert.notEqual(input['a0'], true, 'Safe input');
 	assert.notEqual(output['a0'], true, 'Safe output');
+});
+
+Pollution('prototype', () => {
+	const payload = '{"prototype":{"hello":"world"}}';
+	const input = JSON.parse(payload);
+	const output = klona(input);
+
+	assert.equal(
+		JSON.stringify(output),
+		payload
+	);
+
+	assert.notEqual(({})['hello'], 'world', 'Safe POJO');
+	assert.notEqual(new Object()['hello'], 'world', 'Safe Object');
+
+	assert.notEqual(input['hello'], 'world', 'Safe input');
+	assert.notEqual(output['hello'], 'world', 'Safe output');
 });
 
 Pollution.run();
