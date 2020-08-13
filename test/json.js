a64c3fdd896628c4b315f98535cf8b24bc153dbd
Luke Edwards <luke.edwards05@gmail.com>
2020-08-13 16:02:33 -0700

    chore: add "lite" and "json" tests

---
+++ test/json.js
@@ -0,0 +1,12 @@
+import * as suites from './suites';
+import { klona } from '../src/json';
+
+suites.API(klona);
+
+suites.Strings(klona);
+suites.Booleans(klona);
+suites.Numbers(klona);
+suites.Nully(klona);
+
+suites.Objects(klona);
+suites.Arrays(klona);
