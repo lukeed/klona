107e22fd5849f42ff3dc132bb955fcfc15e2eb3a
Luke Edwards <luke.edwards05@gmail.com>
2020-08-17 11:51:09 -0700

    fix(types): define type module per entry;
    - Closes #21

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,15 @@
-export function klona<T>(input: T): T;
+declare module 'klona' {
+	export function klona<T>(input: T): T;
+}
+
+declare module 'klona/lite' {
+	export function klona<T>(input: T): T;
+}
+
+declare module 'klona/json' {
+	export function klona<T>(input: T): T;
+}
+
+declare module 'klona/full' {
+	export function klona<T>(input: T): T;
+}
