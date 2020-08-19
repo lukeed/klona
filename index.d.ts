71f841505526615f3063fc5c36ca1d4cb179a724
André Antônio Posso da Silva <tripodsgames@users.noreply.github.com>
2020-08-19 01:13:57 -0300

    fix(types): handle multi-module definitions correctly (#22)
    * Fix typing.
    
    * chore: remove manual copies
    
    * chore: use postbuild script
    
    Co-authored-by: Luke Edwards <luke.edwards05@gmail.com>

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,1 @@
-declare module 'klona' {
-	export function klona<T>(input: T): T;
-}
-
-declare module 'klona/lite' {
-	export function klona<T>(input: T): T;
-}
-
-declare module 'klona/json' {
-	export function klona<T>(input: T): T;
-}
-
-declare module 'klona/full' {
-	export function klona<T>(input: T): T;
-}
+export function klona<T>(input: T): T;
